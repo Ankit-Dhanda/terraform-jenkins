e984c5809ce9faf37224665e6006e9a411219274
Ankit Dhanda <ankit_dhanda401@live.in>
2023-03-14 16:41:18 +0530

    Create resources.tf

---
+++ resources.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "s1" {
+  ami = "ami-0329eac6c5240c99d"
+  instance_type = var.instance_type
+  
+  tags = {
+    Name = var.name
+    Env = var.env
+    Owner = var.owner
+  }
+}
